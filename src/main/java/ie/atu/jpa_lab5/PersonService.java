c13e3c615097d36fb8ac999e3ba6d0dca9f3893c
DONALOMAOILCHIARAIN- <G00365724@atu.ie>
2025-11-07 10:37:18 +0000

    REST Get

--- src/main/java/ie/atu/jpa_lab5/PersonService.java
+++ src/main/java/ie/atu/jpa_lab5/PersonService.java
@@ -1,6 +1,8 @@
 package ie.atu.jpa_lab5;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class PersonService {
@@ -8,4 +10,9 @@ public class PersonService {
     public PersonService(PersonRepository repo) {this.repo = repo;} // Denpendency injection
 
     public Person create(Person p) {return repo.save(p);}
+
+    public Person findByEmployeeId(String id){
+        return repo.findByEmployeeId(id)
+                .orElseThrow(() -> new IllegalArgumentException("Person not Found"));
+    }
 }
