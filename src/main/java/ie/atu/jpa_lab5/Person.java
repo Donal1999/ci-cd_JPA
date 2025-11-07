e002eb3c675f673dcbf95febcf9ca6ce23dab464
DONALOMAOILCHIARAIN- <G00365724@atu.ie>
2025-11-07 10:01:15 +0000

    REST post

--- src/main/java/ie/atu/jpa_lab5/Person.java
+++ src/main/java/ie/atu/jpa_lab5/Person.java
@@ -13,11 +13,59 @@ import lombok.*;
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;// primary key
     private String name;
     private String email;
     private String employeeId;
     private String position;
     private String department;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
 
