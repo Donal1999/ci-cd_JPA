628ae6304e9b1e8979789cf871ec93bd44133369
DONALOMAOILCHIARAIN- <g00365724@atu.ie>
2025-11-07 10:37:18 +0000

    REST Get

--- src/main/java/ie/atu/jpa_lab5/PersonController.java
+++ src/main/java/ie/atu/jpa_lab5/PersonController.java
@@ -15,4 +15,9 @@ public class PersonController {
     public Person create(@Valid @RequestBody Person person){
         return service.create(person);
     }
+
+    @GetMapping("/{id}")
+    public Person byEmployeeId(@PathVariable String id){
+        return service.findByEmployeeId(id);
+    }
 }
